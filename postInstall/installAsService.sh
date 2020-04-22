8ebe6d99e30a8be0441c870ecf6f9cff0a938d18
John Rucker <JHRucker@outlook.com>
2020-04-22 08:36:17 -0500

    fix gauge name in install files

--- postInstall/installAsService.sh
+++ postInstall/installAsService.sh
@@ -2,17 +2,17 @@
 # From DOS prompt type (git update-index --chmod=+x installAsService.sh) to make this file executable.
 set -e
 echo "NPM post install shell that installs this app as service starts now..."
-echo "Set irdclient as defalut group for testGauge -> sudo chown :irdclient ../testGauge"
-sudo chown :irdclient ../testGauge
-echo "Give default group write access to the testGauge directory -> sudo chmod g+w ../testGauge"
-sudo chmod g+w ../testGauge
-echo "Install D-Bus config file for this service -> sudo cp ./postInstall/dbus.conf /etc/dbus-1/system.d/testGauge.conf"
-sudo cp ./postInstall/dbus.conf /etc/dbus-1/system.d/testGauge.conf
-echo "Install systemd service file -> sudo cp -n ./postInstall/server.service /etc/systemd/system/testGauge.service"
-sudo cp -n ./postInstall/server.service /etc/systemd/system/testGauge.service
-echo "Enable the servers to start on reboot -> systemctl enable testGauge.service"
-sudo systemctl enable testGauge.service
-echo "Start the service now -> systemctl start testGauge.service"
-sudo systemctl start testGauge.service
+echo "Set irdclient as defalut group for TestGauge -> sudo chown :irdclient ../TestGauge"
+sudo chown :irdclient ../TestGauge
+echo "Give default group write access to the TestGauge directory -> sudo chmod g+w ../TestGauge"
+sudo chmod g+w ../TestGauge
+echo "Install D-Bus config file for this service -> sudo cp ./postInstall/dbus.conf /etc/dbus-1/system.d/TestGauge.conf"
+sudo cp ./postInstall/dbus.conf /etc/dbus-1/system.d/TestGauge.conf
+echo "Install systemd service file -> sudo cp -n ./postInstall/server.service /etc/systemd/system/TestGauge.service"
+sudo cp -n ./postInstall/server.service /etc/systemd/system/TestGauge.service
+echo "Enable the servers to start on reboot -> systemctl enable TestGauge.service"
+sudo systemctl enable TestGauge.service
+echo "Start the service now -> systemctl start TestGauge.service"
+sudo systemctl start TestGauge.service
 echo "NPM Post install shell is complete."
-#echo "To start this servers please reboot the server. After reboot Type -> journalctl -u testGauge -f <- to see status."
+#echo "To start this servers please reboot the server. After reboot Type -> journalctl -u TestGauge -f <- to see status."
--- postInstall/un-installAsService.sh
+++ postInstall/un-installAsService.sh
@@ -3,12 +3,12 @@
 # From DOS prompt type (git update-index --chmod=+x un-installAsService.sh) to make this file executable.
 # set -e  <-This will cause script to end on first error with error code
 echo "un-install service will stop and remove this app as a service starts now..."
-echo "Stop service -> systemctl stop testGauge.service"
-sudo systemctl stop testGauge.service
-echo "Disable servers so it will not start on reboot ->systemctl disable testGauge.service"
-sudo systemctl disable testGauge.service
-echo "Remove the systemd service file -> rm /etc/systemd/system/testGauge.service"
-sudo rm /etc/systemd/system/testGauge.service
-echo "Remove D-Bus config file sudo rm /etc/dbus-1/system.d/testGauge.conf"
-sudo rm /etc/dbus-1/system.d/testGauge.conf
+echo "Stop service -> systemctl stop TestGauge.service"
+sudo systemctl stop TestGauge.service
+echo "Disable servers so it will not start on reboot ->systemctl disable TestGauge.service"
+sudo systemctl disable TestGauge.service
+echo "Remove the systemd service file -> rm /etc/systemd/system/TestGauge.service"
+sudo rm /etc/systemd/system/TestGauge.service
+echo "Remove D-Bus config file sudo rm /etc/dbus-1/system.d/TestGauge.conf"
+sudo rm /etc/dbus-1/system.d/TestGauge.conf
 echo "un-instal shell is complete. Reboot server to stop service."
